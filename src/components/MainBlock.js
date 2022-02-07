ce48865b0efadd881b6a2cbdd64a26dc304b51cb
kingqabil <kingqabil@live.com>
2022-02-08 02:12:02 +0300

    Add odd even block rendering

--- src/components/MainBlock.js
+++ src/components/MainBlock.js
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 import { BsArrowRightCircle } from 'react-icons/bs';
 
 const MainBlock = props => {
-    
+
     let curmetric = [props]
     let image = `/all/${props.obj.CountryCode.toLowerCase()}/vector.svg`;
+    let colClass;
+    if (props.index % 2 == 1) {
+      colClass = 'evencol'
+    }else{
+      colClass = 'oddcol'
+    }
 
   return (
       <>
     <Link to={`/infopage/${props.obj.Country}`}>
+    <div className="">{props.index % 2 == 1 ? console.log('hi') : null}</div>
           <BsArrowRightCircle className='toInfoCircle' />
-      <div className="mainCountryBlock">
+          <div className={`mainCountryBlock ${colClass}`}>
         <img
           className="countryImg"
           src={image}
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,6 +3,7 @@ import { fetchCountryInfo, getAllData } from '../API'
 import { useDispatch,useSelector } from "react-redux";
 import MainBlock from '../components/MainBlock';
 import MainHeaderBlock from '../components/MainHeaderBlock';
+import { v4 as uuidv4 } from 'uuid';
 
 
 const Main = () => {
@@ -12,20 +13,20 @@ const Main = () => {
    //const [metric, setMetric] = useState('TotalConfirmed')
    const [countriesupdated, setcountriesupdated] = useState(false)
    if (countriesupdated == true) {
-       
+
        setcountriesupdated(false)
    }
-    
+
     useEffect(() => {
         dispatch(fetchCountryInfo())
 
       },[]);
       let countries = useSelector((state) => state);
       //setCountryState(countries)
-      
+
       const changeMetric = (e) => {
        let metric = e.target.value
-       
+
        setcountriesupdated(metric)
       updateList()
     }
@@ -41,25 +42,27 @@ const Main = () => {
         <div>
             <MainHeaderBlock />
             <div className="indicationStripe">
-                stats sorted by: 
-                <select onChange={changeMetric} name="" id="">
+            <div className="indicationStripeText">stats sorted by: </div>
+                <div className="selects">
+                <select onChange={changeMetric} >
                     <option value="TotalConfirmed">Total cases</option>
                     <option value="TotalDeaths">Total deaths</option>
                     <option value="NewConfirmed">New cases</option>
                     <option value="NewDeaths">New deaths</option>
                 </select>
-                <select onChange={changeOrder} name="" id="">
+                <select onChange={changeOrder} >
                     <option value='normal'>high to low</option>
                     <option value='reversed'>Low to high</option>
                 </select>
+                </div>
             </div>
             <div className='mainCountryWrapper'>
-          {countries.map((countrObj) => 
-              <MainBlock obj={countrObj} curmetric={countriesupdated} />
+            {countries.map((countrObj,index) =>
+              <MainBlock index={index} obj={countrObj} key={uuidv4()} curmetric={countriesupdated} />
           )}
           </div>
           hello
-          
+
         </div>
     )
 }
