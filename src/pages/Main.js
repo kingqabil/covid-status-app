b0bc27e10ce5d95151843e187acb916cd889cd58
kingqabil <kingqabil@live.com>
2022-02-08 00:21:50 +0300

    Add styling

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { fetchCountryInfo, getAllData  } from '../API';
 import { useDispatch, useSelector } from 'react-redux';
 import MainBlock from '../components/MainBlock';
+import MainHeaderBlock from '../components/MainHeaderBlock';
 
 const Main = () => {
   const dispatch = useDispatch();
