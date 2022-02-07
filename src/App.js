a8bd763ccf588e3c691c784528e5256dd516d2cd
kingqabil <kingqabil@live.com>
2022-02-08 00:59:51 +0300

    Add basic header

--- src/App.js
+++ src/App.js
@@ -6,22 +6,29 @@ import reactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import InfoPage from './pages/InfoPage';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div className="App">
-      <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/infopage/:country" element={<InfoPage />}>
+    <Provider store={store}>
+  <BrowserRouter>
+   <Routes>
+     <Route path="/" element={<><Navbar page='Country data' /><Main /></>}>
+       <Route index element={<Navbar />} />
 
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      </Provider>
-    </div>
-  );
+     </Route>
+     <Route path="/infopage/:country" element={<><Navbar page='Country data' /><InfoPage /></>}>
+       {/* <Route index element={<Main />} /> */}
+
+     </Route>
+   </Routes>
+ </BrowserRouter>
+ </Provider>
+ </div>
+);
 }
+
 
 export default App;
