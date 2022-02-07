270297ca1016e9dc2e6bacb048daa6a08fa4d427
kingqabil <kingqabil@live.com>
2022-02-08 01:10:57 +0300

    Add navbar props

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Navbar = () => {
-    return (
-        <nav>
-            hello0000000
-        </nav>
+const Navbar = (props) => {
+  return (
+      <nav>
+          <div className="left">back</div>
+          <div className="right">{props.page}</div>
+          <div className="right">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+      </nav>
     )
 }
 
